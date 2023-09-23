38085596164557908492dc8c162700125ac2251c
matsu2254 <matsu2254@gmail.com>
2023-09-23 09:12:15 +0900

    mod variables

--- code/mt5/main.py
+++ code/mt5/main.py
@@ -2,7 +2,6 @@
 import datetime 
 from time import sleep
 import schedule
-import pandas as pd
 from dateutil import tz
 
 import MetaTrader5 as mt5
@@ -10,13 +9,17 @@ import MetaTrader5 as mt5
 import dbcall
 import uploadinfluxdb
 
-token = '78mdMPrH02UaKsrsb6Q2Ofj0neRVfRNDaFfrN2RWqreobbh3RtP7jyKX9-Ktvt-JBAthd1FPnZggkRANHi9T8w=='
-org = "influxdata"
-bucket = "test"
-url = "http://192.168.0.15"
+TOKEN = '78mdMPrH02UaKsrsb6Q2Ofj0neRVfRNDaFfrN2RWqreobbh3RtP7jyKX9-Ktvt-JBAthd1FPnZggkRANHi9T8w=='
+ORG = "influxdata"
+BUCKET = "test"
+URL = "http://192.168.0.15"
+SYMBOL = "EURJPY"
+TF=mt5.TIMEFRAME_H4
+REP=20
 
 
 start=datetime.datetime.now(tz=tz.gettz('Asia/Tokyo'))-datetime.timedelta(hours=1)
+
 #ticks = get_ticks(symbol="USDJPY",start=start)
 #bars = get_bars(symbol="USDJPY",tf=mt5.TIMEFRAME_H4,start=start)
 
@@ -24,19 +27,27 @@ start=datetime.datetime.now(tz=tz.gettz('Asia/Tokyo'))-datetime.timedelta(hours=
 #print ( bars)
  
  # 確認
-h=dbcall.db_call(url=url,token=token,org=org,bucket=bucket)
+h=dbcall.db_call(
+                url=URL,
+                token=TOKEN,
+                org=ORG,
+                bucket=BUCKET
+                )
 #
 #print(h.get_last_time("GBPJPY"))
 #print(h.get_dataframe(measurement="GBPJPY",data_type="ohlc",start="2022-12-01",stop="2023-01-01"))
 #
 #h.write_dataframe(data=bars,mj="USDJPY",tag="Smybol")
 
-h.write_dataframe(data=get_ticks(symbol="AUDJPY",start=start),measurement="AUDJPY",tag="Smybol")
+h.write_dataframe(data=uploadinfluxdb.get_ticks(symbol=SYMBOL,start=start),
+                  measurement=SYMBOL,
+                  tag="Smybol"
+                  )
 
 
 def upload_task():
-    SYM="AUDJPY"
-    RATE=mt5.TIMEFRAME_H4
+    SYM=SYMBOL
+    TIMEFRAME=TF
     last=None
     ticks=None
     #try:
@@ -44,19 +55,21 @@ def upload_task():
     #except Exception as e:
     #    print("except get lasttime")
  #       raise e
+    
     print("execute task")
+
     if not last:
         last=datetime.datetime.now(tz=tz.gettz('Asia/Tokyo'))-datetime.timedelta(minutes=1)
 
     ticks = uploadinfluxdb.get_ticks(symbol=SYM,start=last)
-    #bars = get_bars(symbol=SYM,tf=RATE,start=start)
+    #bars = uploadinfluxdb.get_bars(symbol=SYM,tf=TIMEFRAME,start=start)
     print(last)
     print(ticks)
     #h.write_dataframe(data=bars,mj=SYM,tag="Symbol")
     h.write_dataframe(data=ticks,measurement=SYM,tag="Smybol")
 
 
-schedule.every(20).seconds.do(upload_task)
+schedule.every(REP).seconds.do(upload_task)
 
 while True:
 	schedule.run_pending()
