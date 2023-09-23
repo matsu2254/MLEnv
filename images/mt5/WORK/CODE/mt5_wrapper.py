81e3ddf621701829189a56664e782385c3feb4bf
kazuki matsushita <you@example.com>
2023-09-23 13:51:32 +0900

    update image for mt5 data upload

---
+++ images/mt5/WORK/CODE/mt5_wrapper.py
@@ -0,0 +1,58 @@
+#from datetime import datetime
+import datetime 
+import pandas as pd
+
+import MetaTrader5 as mt5
+from dateutil import tz
+
+
+
+def get_ticks(symbol: str,start :datetime) -> pd.DataFrame:
+   if not mt5.initialize():
+      print("initialize() failed")
+      mt5.shutdown()
+      return None
+   now = datetime.datetime.now(tz=tz.gettz('Asia/Tokyo'))
+   try:
+      ticks = pd.DataFrame(mt5.copy_ticks_range(symbol, start, now, mt5.COPY_TICKS_ALL))
+   except Exception as e:
+      print(e)
+      try:
+         ticks = pd.DataFrame(mt5.copy_ticks_range(symbol, now - datetime.timedelta(days=30), now, mt5.COPY_TICKS_ALL))
+      except Exception as e:
+         print(e)
+         raise Exception
+   finally:
+      mt5.shutdown()
+   ticks['time_msc'] = pd.to_datetime(ticks['time_msc'],unit='ms',origin='unix')
+   ticks = ticks.set_index('time_msc')
+   ticks = ticks.drop(['time','last','volume','volume_real'],axis=1)
+   ticks.columns = ['Bid','Ask','Flags']
+   ticks['Symbol']=symbol
+   return ticks
+
+def get_bars(symbol: str,tf,start :datetime) -> pd.DataFrame:
+   # TODO
+   # add column for describe datafame
+   if not mt5.initialize():
+      print("initialize() failed")
+      mt5.shutdown()
+      return None
+   now = datetime.datetime.now(tz=tz.gettz('Asia/Tokyo'))
+   try:
+      bars = pd.DataFrame(mt5.copy_rates_range(symbol, tf, start, now, ))
+   except Exception as e:
+      print(e)
+      try:
+         bars = pd.DataFrame(mt5.copy_rates_range(symbol, tf, now - datetime.timedelta(days=30), now, mt5.COPY_TICKS_ALL))
+      except Exception as e:
+         print(e)
+         raise Exception
+   finally:
+      mt5.shutdown()
+   bars['time'] = pd.to_datetime(bars['time'],unit='s',origin='unix')
+   bars = bars.set_index('time')
+   bars.columns = ['Open','High','Low','Close','Tick_olume','Spread','Real_volume']
+   bars['Symbol']=symbol
+   return bars
+
